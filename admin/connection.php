de70c079ee6ada1be07d45e8a4bd3d6bab616921
mohamed zaitoune <mohamedztn37@gmail.com>
2023-02-12 16:23:20 +0100

    start changing code to pdo

--- admin/connection.php
+++ admin/connection.php
@@ -1,2 +1,15 @@
 <?php
-    $con=mysqli_connect("localhost","root","","order_food_project");
+    // $con=mysqli_connect("localhost","root","","order_food_project");
+
+    function pdo_connect_mysql() {
+        $DATABASE_HOST = 'localhost';
+        $DATABASE_USER = 'root';
+        $DATABASE_PASS = '';
+        $DATABASE_NAME = 'order_food_project';
+        try {
+            return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+        } catch (PDOException $exception) {
+            exit('Failed to connect to database!');
+        }
+    }
+$pdo = pdo_connect_mysql();
