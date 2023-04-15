4d7f11376356fe4be5af33fe0d86ed9eff5aa5ff
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-04-15 12:06:16 +0000

    add admin lte assets

---
+++ admin/includes/footer.php
@@ -0,0 +1,43 @@
+</div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="../admin/assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstra../admin/assets/p -->
+<script src="../admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayS../admin/assets/crollbars -->
+<script src="../admin/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE../admin/assets/ App -->
+<script src="../admin/assets/dist/js/adminlte.js"></script>
+<!-- PAGE PLU../admin/assets/GINS -->
+<!-- jQuery M../admin/assets/apael -->
+<script src="../admin/assets/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="../admin/assets/plugins/raphael/raphael.min.js"></script>
+<script src="../admin/assets/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="../admin/assets/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS ../admin/assets/-->
+<script src="../admin/assets/plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE../admin/assets/ for demo purposes -->
+<script src="../admin/assets/dist/js/demo.js"></script>
+<!-- AdminLTE../admin/assets/ dashboard demo (This is only for demo purposes) -->
+<script src="../admin/assets/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
--- admin/login.php
+++ admin/login.php
@@ -1,56 +1,95 @@
 <?php
-    session_start();
-    // ob_start();
-    include ('connection.php');
-    if (isset($_SESSION['admin_username'])) {
-        header("Location: index.php");
+session_start();
+// ob_start();
+include('connection.php');
+if (isset($_SESSION['admin_username'])) {
+    header("Location:index.php");
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $query = "SELECT * FROM admin_table WHERE username='$username' AND password ='$password'";
+    $sql = $pdo->prepare($query);
+    $sql->execute();
+
+    if ($sql->rowCount() == 1) {
+        $sql = $pdo->prepare("SELECT * FROM admin_table WHERE username='$username' AND password ='$password'");
+        $sql->execute();
+        $rows = $sql->fetch();
+        $_SESSION['admin_username'] = $rows['username'];
+        $_SESSION['admin_id'] = $rows['id'];
+        header("Location:index.php");
     }
-    
-    if (isset($_POST['submit'])) {
-            $username = $_POST['username'];
-            $password =md5($_POST['password']);
-            $sql = "SELECT * FROM admin_table WHERE username='$username' AND password='$password'";
-            $result = mysqli_query($con, $sql);
-            if ($result->num_rows > 0) {
-                $row = mysqli_fetch_assoc($result);
-                $_SESSION['admin_username'] = $row['username'];
-                $_SESSION["admin_fullname"]= $row['full_name']; 
-                header("Location:index.php");
-            } else {
-                echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
-            }
-    }
+}
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <script src="https://kit.fontawesome.com/f9a469bd95.js" crossorigin="anonymous"></script>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="log_in.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in (v2)</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../admin/assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="../admin/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->*
+    <link rel="stylesheet" href="../admin/assets/dist/css/adminlte.min.css">
 </head>
 
-<body>
-    <div class="singupform">
-        <form class="form" method="post">
-            <h1>Log In</h1>
-            <div class="inputcontainer">
-                <input type="text" placeholder="a" class="input" id="email_input" name="username">
-                <label for="" class="label">Email Or Username</label>
+<body class="hold-transition login-page" style="background-color: black;">
+    <div class="login-box" >
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="" class="h1"><b>Admin</b></a>
             </div>
-            <div class="inputcontainer">
-                <input type="password" placeholder="a" class="input" id="password_input" name="password">
-                <label for="" class="label">Password</label>
-                <br><br><br>
-            </div> 
-                <input type="submit" class="submitbtn" value="Log In" name="submit">
-        </form>
-        <button id="mybtn">sh</button>
+            <div class="card-body">
+
+                <form action="" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Email" name="username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
     </div>
-    <script src="log_in.js"></script>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="../admin/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../admin/assets/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
